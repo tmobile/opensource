adbfb4d97b216293e75a984adf6055d9b76ad1b6
Abhishek <abhishekanand18@gmail.com>
2017-09-11 23:36:47 -0700

    Design changes + static blog page.

--- themes/tmo-oss-theme/static/js/main.js
+++ themes/tmo-oss-theme/static/js/main.js
@@ -1,28 +1,27 @@
-function navigateTo(url){
-    window.location.href = url;
+function navigateTo(url) {
+  window.location.href = url;
 }
 
 function openPageInModal(href) {
-	var jqOverlay;
+  var jqOverlay;
 
-    $('body').addClass('overflowH');
-	jqOverlay = $('#overlay-container-services');
-    jqOverlay.removeClass('display-none');
+  $('body').addClass('overflowH');
+  jqOverlay = $('#overlay-container-services');
+  jqOverlay.removeClass('display-none');
 
-    jqOverlay.find('.event-container').load(href, function() {
-		// On load, move first found H1 to modal title
-		var jqTitle = jqOverlay.find('.event-container h1:first').remove();
-		jqOverlay.find('.success-header').html(jqTitle.html());
-	});
+  jqOverlay.find('.event-container').load(href, function () {
+    // On load, move first found H1 to modal title
+    var jqTitle = jqOverlay.find('.event-container h1:first').remove();
+    jqOverlay.find('.success-header').html(jqTitle.html());
+  });
 }
 
-$(document).on("click", ".animated-scroll", function(e) {
-    e.preventDefault();
-    var id = $(this).attr("href"),
+$(document).on("click", ".animated-scroll", function (e) {
+  e.preventDefault();
+  var id = $(this).attr("href"),
     topSpace = 30;
-        
-    $('html, body').animate({
-          scrollTop: $(id).offset().top - topSpace
-    }, 800);
-});
 
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - topSpace
+  }, 800);
+});

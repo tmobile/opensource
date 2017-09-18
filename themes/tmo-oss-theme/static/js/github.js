4916c54874117149a179adb5d722b51b2cee9041
Ram Gopinathan <rprakashg@outlook.com>
2017-09-17 23:03:39 -0700

    added jquery support to pull stars and forks info from github dynamically

--- themes/tmo-oss-theme/static/js/github.js
+++ themes/tmo-oss-theme/static/js/github.js
@@ -5,30 +5,25 @@
         type: "GET",
         cache: true,
         success: function(repos) {
-            $("input[name='projectName']").each(function(){
-                var name = $(this).val();
-                var repo = $.grep(repos, function(r){
-                    return r.name == name;
+            var projectRow = $(".project-row");
+            if(projectRow) {
+                $(projectRow).find(".markup").each(function(){
+                    var name = $(this).find(".markup-text").text();
+                    var repo = $.grep(repos, function(r){
+                        return r.name == name;
+                    })
+                    if(repo && repo.length == 1){
+                        var footer = $(this).find(".markup-footer");
+                        if(footer) {
+                            var elms = $(footer).children();
+                            if(elms != null && elms.length == 2){
+                                $(elms[0]).prepend(repo[0].watchers);
+                                $(elms[1]).prepend(repo[0].forks);
+                            }
+                        }
+                    }
                 })
-                if(repo && repo.length == 1){
-                    var desc = $("#" + name + "_description");
-                    var forks = $("#" + name + "_forks");
-                    var stars = $("#" + name + "_stars")
-
-                    if(desc){
-                        //set description
-                        desc.text(repo[0].description);
-                    }
-                    if(forks){
-                        //set forks
-                        forks.text(forks.text() + repo[0].forks)
-                    }
-                    if(stars){
-                        //set stars
-                        stars.text(stars.text() + repo[0].watchers)
-                    }
-                }
-            })
+            }
         }
     })
 })(jQuery); // End of use strict
